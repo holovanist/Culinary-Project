4051def354d653332cce8a93bce2cfa4b7a7fdf6
holovanist <jonsonlogan37@gmail.com>
2024-10-21 13:54:59 -0700

    first test of design

--- Culinary project/Assets/scripts/GetInputFieldValue.cs
+++ Culinary project/Assets/scripts/GetInputFieldValue.cs
@@ -8,13 +8,6 @@ public class GetInputFieldValue : MonoBehaviour
     TMP_InputField i_flourAmt;
     [SerializeField]
     TMP_InputField i_flourCost;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
