aed99467eefe648bb977a545fe42972fbfc2aa81
holovanist <jonsonlogan37@gmail.com>
2024-10-23 13:04:09 -0700

    tested different code

--- Culinary project/Assets/scripts/Equation.cs
+++ Culinary project/Assets/scripts/Equation.cs
@@ -5,42 +5,16 @@ using UnityEngine;
 
 public class Equation : MonoBehaviour
 {
-    [SerializeField]
-    TMP_InputField field;
-    [SerializeField]
-    TMP_InputField i_flourAmt;
-    [SerializeField]
-    TMP_InputField i_flourCost;
-    [SerializeField]
-    TMP_InputField i_1a;
-    [SerializeField]
-    TMP_InputField i_2a;
-    [SerializeField]
-    TMP_InputField i_1b;
-    [SerializeField]
-    TMP_InputField i_2b;
-    [SerializeField]
-    TMP_InputField i_1c;
-    [SerializeField]
-    TMP_InputField i_2c;
+    float test;
     public void Calculate()
     {
-
-        float flourCost = -1;
-        Debug.Log(float.TryParse(i_flourCost.text, out flourCost));
-        float flourAmt = -1;
-        Debug.Log(float.TryParse(i_flourAmt.text, out flourAmt));
-        Debug.Log(flourAmt * flourCost);
+        //TMP_InputField[] foo = GameObject.FindObjectsOfType<TMP_InputField>();
+        //GameObject[] cost = GameObject.FindGameObjectsWithTag("Cost");
+        //GameObject[] Amt = GameObject.FindGameObjectsWithTag("Amount");
+        //TMP_InputField[] a = TMP_InputField.FindObjectsOfType<TMP_InputField>();
+        //Debug.Log(float.TryParse(a, out test));
     }
-    /*using System;
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
-using UnityEngine;
-
-public class List : MonoBehaviour
-{
-
+    /*
     List<TMP_InputField> Ingrediant;
     //use list to tell haw many ingrediants ther are
     //calculate using the list
@@ -50,14 +24,5 @@ public class List : MonoBehaviour
     {
 
     }
-    public void Calculate()
-    {
-        //TMP_InputField[] foo = GameObject.FindObjectsOfType<TMP_InputField>();
-        
-                
-        
-        Debug.Log(ToString(Ingrediant));
-    }
-}
 */
 }
